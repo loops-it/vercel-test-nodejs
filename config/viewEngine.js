d8599e0c39ecdb925f7e411563de2c973f7d17ff
madushan10 <mnmnjith@gmail.com>
2023-05-03 15:54:09 +0530

    commit 4

--- config/viewEngine.js
+++ config/viewEngine.js
@@ -6,7 +6,6 @@ Config view engine for node app
 
 let configViewEngine = (app) => {
     app.use(express.static("../public"));
-    app.set("view engine", "ejs");
     app.set("views","../views");
 };
 
